fc7ec1c28c28221784f9c8c89c233dce74e29e86
SaJo97 <paybacko90@gmail.com>
2024-12-17 16:58:18 +0100

    Lägg till dynamisk bild(html,css,js). meny-sidan

--- Meny.js
+++ Meny.js
@@ -65,3 +65,48 @@ favoriteButton.addEventListener('click', () => {
 })
 .catch(error => console.error('Error loading menu data:', error));
 })
+
+// DYNAMISK FUNKTION
+
+let slideIndex = 0;
+let slideTimeout; // timeout ID
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  let thumbnails = document.getElementsByClassName("demo");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    thumbnails[i].classList.remove("active");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  thumbnails[slideIndex - 1].classList.add("active");
+
+  clearTimeout(slideTimeout);
+  slideTimeout = setTimeout(showSlides, 3000);
+}
+
+function plusSlides(n) {
+  slideIndex += n - 1;
+  let slides = document.getElementsByClassName("mySlides"); //DOM slide access
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1; //last side
+  }
+  if (slideIndex >= slides.length) {
+    slideIndex = 0; //go first slide
+  }
+  showSlides();
+}
+
+function currentSlide(n) {
+  slideIndex = n - 1;
+  showSlides();
+}
+
+showSlides();
